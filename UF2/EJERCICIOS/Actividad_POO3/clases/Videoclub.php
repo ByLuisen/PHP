fa610e8d0505f09ba570e830635c91d8e8f21cd0
Luis_Enrique_Ledesma_Ollague <luisenric32@gmail.com>
2023-12-02 15:39:50 +0100

    Añadir métodos a la clase Videoclub

--- UF2/EJERCICIOS/Actividad_POO3/clases/Videoclub.php
+++ UF2/EJERCICIOS/Actividad_POO3/clases/Videoclub.php
@@ -8,4 +8,17 @@ class Videoclub {
     /** @var Cliente[] */
     private array $socios = [];
     private int $numSocios;
+
+    public function __construct($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    private function incluirProducto(Soporte $producto): void {
+        $this->productos[] = $producto;
+    }
+
+    public function incluirCintaVideo(string $titulo, float $precio, int $duracion) {
+        $cintaVideo = new CintaVideo($titulo, $this->numProductos++, $precio, $duracion);
+    }
 }

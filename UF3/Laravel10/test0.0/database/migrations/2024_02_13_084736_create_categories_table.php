db337bbbe3c694d73ed432599ea4b66c5eee6b28
Luis_Enrique_Ledesma_Ollague <luisenric32@gmail.com>
2024-02-13 09:55:15 +0100

    Migrations

--- UF3/Laravel10/test0.0/database/migrations/2024_02_13_084736_create_categories_table.php
+++ UF3/Laravel10/test0.0/database/migrations/2024_02_13_084736_create_categories_table.php
@@ -1,5 +1,4 @@
 <?php
-// control de versiones de las bases de datos
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -12,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 45);
+            $table->string('slug');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
